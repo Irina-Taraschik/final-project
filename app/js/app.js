be559104f8686a0c8016ace61662fd3100ff6c53
Irina-Taraschik <livefortoday@mail.ru>
2021-11-28 13:40:19 +0300

    3 sliders made

--- app/js/app.js
+++ app/js/app.js
@@ -2,9 +2,11 @@
 
 // require('~/app/libs/mmenu/js/jquery.mmenu.all.min.js') // import vendor jQuery plugin example (not module)
 
+
+
 document.addEventListener('DOMContentLoaded', () => {
 	
-	const items = [...document.querySelectorAll('.shop__item')]
+	const items = [...document.querySelectorAll('.shop__items')]
 	const left = document.querySelector('#left')
 	const right = document.querySelector('#right')
 	const count = document.querySelector('.shop__arrow-number')
@@ -49,3 +51,99 @@ document.addEventListener('DOMContentLoaded', () => {
 	})
 
 	})
+
+document.addEventListener('DOMContentLoaded', () => {
+	
+	const itemsNew = [...document.querySelectorAll('.new__items')]
+	const leftNew = document.querySelector('#leftNew')
+	const rightNew = document.querySelector('#rightNew')
+	const countNew = document.querySelector('.new__arrow-number')
+		
+	let el = 0;
+	function firstRender(params) {
+		itemsNew.forEach((el, i) => {
+			if (i === 0) {
+				el.classList.add('active')
+			}
+		})
+		countNew.textContent = el + 1
+	}
+	firstRender();
+	
+	function changeSlide() {
+			
+		itemsNew.forEach((el) => {
+			el.classList.remove('active')
+		})
+		itemsNew[el].classList.add('active')
+		countNew.textContent = el + 1
+	}
+	
+	leftNew.addEventListener('click', (event) => {
+		if (!event.isTrusted) return; //for webpack
+		console.log('left', el);
+		if (el === 0) {
+			el = itemsNew.length - 1
+		} else {
+			el = el - 1
+		}
+		changeSlide()
+	})
+	
+	rightNew.addEventListener('click', (event) => {
+		if (!event.isTrusted) return; //for webpack
+		console.log('right', el);
+		if (el === itemsNew.length - 1) el = 0
+		else el = el + 1
+		changeSlide()
+	})
+	
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+	
+	const itemsStories = [...document.querySelectorAll('.stories__items')]
+	const leftStories = document.querySelector('#leftStories')
+	const rightStories = document.querySelector('#rightStories')
+	const countStories = document.querySelector('.stories__arrow-number')
+		
+	let el = 0;
+	function firstRender(params) {
+		itemsStories.forEach((el, i) => {
+			if (i === 0) {
+				el.classList.add('active')
+			}
+		})
+		countStories.textContent = el + 1
+	}
+	firstRender();
+	
+	function changeSlide() {
+			
+		itemsStories.forEach((el) => {
+			el.classList.remove('active')
+		})
+		itemsStories[el].classList.add('active')
+		countStories.textContent = el + 1
+	}
+	
+	leftStories.addEventListener('click', (event) => {
+		if (!event.isTrusted) return; //for webpack
+		console.log('left', el);
+		if (el === 0) {
+			el = itemsStories.length - 1
+		} else {
+			el = el - 1
+		}
+		changeSlide()
+	})
+	
+	rightStories.addEventListener('click', (event) => {
+		if (!event.isTrusted) return; //for webpack
+		console.log('right', el);
+		if (el === itemsStories.length - 1) el = 0
+		else el = el + 1
+		changeSlide()
+	})
+	
+})
